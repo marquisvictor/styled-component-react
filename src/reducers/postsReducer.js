5824200e4df65ecd14a6737d58408e7202dbf871
Victor Irekponor <marquisvictor93@gmail.com>
2020-12-21 23:27:40 +0100

    setting up redux actions and consuming them with the reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,5 @@
+import * as actions from '../actions/postsActions';
+
 export const initialState = {
     posts: [],
     loading: false,
@@ -7,6 +9,26 @@ export const initialState = {
 export default function postsReducer(state = initialState, action) {
     const { type } = action;
     switch (type) {
+        case actions.GET_POSTS:
+            return {
+                ...state,
+                loading: true,
+            };
+        case actions.GET_POSTS_SUCCESS:
+            return {
+                // ...state,
+                posts: action.payload,
+                loading: false,
+                // hasErrors: false,
+            };
+
+        case actions.GET_POST_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                hasErrors: true,
+            };
+
         default:
             return state;
     }
