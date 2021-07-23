9e2f1ed26cb0c7f028f5a5408ed09a642a78d2b9
Victor Irekponor <marquisvictor93@gmail.com>
2021-07-24 00:34:00 +0100

    color picker building, atleast here I learnt of a new way to write a ternary operator without using the ? just using && only. Its in the code

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import SingleColor from './SingleColor';
 import Values from 'values.js';
 
 function App() {
-    const [color, setColor] = useState('');
+    const [color, setColor] = useState();
     const [error, setError] = useState(false);
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(new Values('#f15025').all(20));
 
     function handleSubmit(e) {
         e.preventDefault();
 
         try {
             let colors = new Values(color).all(10);
-            console.log(colors);
+            setList(colors);
         } catch (error) {
-            console.log(error);
             setError(true);
         }
     }
@@ -30,12 +29,18 @@ function App() {
                         value={color}
                         onChange={e => setColor(e.target.value)}
                         placeholder='#f15025'
+                        className={error ? 'error' : null}
                     />
 
                     <button type='submit' className='btn'>
                         submit
                     </button>
                 </form>
+            </section>
+            <section className='colors'>
+                {list.map((key, index) => {
+                    return <SingleColor key={index} {...key} index={index} />;
+                })}
             </section>
         </>
     );
