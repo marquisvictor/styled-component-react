3a287f1c6d8269441264a38c62c900555a106155
Victor Irekponor <marquisvictor93@gmail.com>
2021-07-24 21:40:12 +0100

    updates for today's push I don dey tire brov

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
-import React, { useState } from 'react';
-import SingleColor from './SingleColor';
-
-import Values from 'values.js';
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-    const [color, setColor] = useState();
-    const [error, setError] = useState(false);
-    const [list, setList] = useState(new Values('#f15025').all(20));
+    const [name, setName] = useState('');
+    const [list, setList] = useState([]);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editId, setEditId] = useState(null);
+    const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
-    function handleSubmit(e) {
+    const handleSubmit = e => {
         e.preventDefault();
-
-        try {
-            let colors = new Values(color).all(10);
-            setList(colors);
-        } catch (error) {
-            setError(true);
+        if (!name) {
+            // display alert
+        } else if (name && isEditing) {
+            // deal with if editing it true
+        } else {
+            const newItems = {
+                id: new Date().getTime().toString(),
+                title: name,
+            };
+            // console.log(List);
+            setList([...list, newItems]);
+            setName('');
         }
-    }
+    };
+
+    const handleInput = e => {
+        const { value } = e.target;
+        setName(value);
+    };
 
     return (
-        <>
-            <section className='container'>
-                <h3>Color Generator</h3>
-                <form action='' onSubmit={handleSubmit}>
+        <section className='section-center'>
+            <form action='' onSubmit={handleSubmit} className='grocery-form'>
+                {alert.show && <Alert />}
+                <h3>grocery bud</h3>
+                <div className='form-control'>
                     <input
+                        className='grocery'
+                        placeholder='e.g. call victor'
                         type='text'
-                        value={color}
-                        onChange={e => setColor(e.target.value)}
-                        placeholder='#f15025'
-                        className={error ? 'error' : null}
+                        value={name}
+                        onChange={handleInput}
                     />
-
-                    <button type='submit' className='btn'>
-                        submit
+                    <button type='submit' className='submit-btn'>
+                        {isEditing ? 'edit' : 'submii'}
                     </button>
-                </form>
-            </section>
-            <section className='colors'>
-                {list.map((key, index) => {
-                    return <SingleColor key={index} {...key} index={index} />;
-                })}
-            </section>
-        </>
+                </div>
+            </form>
+            <div className='grocery-container'>
+                <List items={list} />
+                <button className='clear-btn'>clear items</button>
+            </div>
+        </section>
     );
 }
 
