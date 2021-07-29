83f4512e138d9582b35050c85ec5368f697a5990
Victor Irekponor <marquisvictor93@gmail.com>
2021-07-29 07:35:21 +0100

    finish navbar tutorials supposedly learnt useRef component, or more like used it. I know I might one day come back to all these PRs and comments I'm writing up and down]

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,17 +4,33 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+    const linkContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    useEffect(() => {
+        const linkHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+            linkContainerRef.current.style.height = `${linkHeight}px`;
+        } else {
+            linkContainerRef.current.style.height = '0px';
+        }
+    }, [showLinks]);
+
     return (
         <nav>
             <div className='nav-center'>
                 <div className='nav-header'>
                     <img src={logo} alt='logo' />
-                    <button className='nav-toggle'>
+                    <button
+                        onClick={() => setShowLinks(!showLinks)}
+                        className='nav-toggle'
+                    >
                         <FaBars />
                     </button>
                 </div>
-                <div className='links-container show-container'>
-                    <ul className='links'>
+                <div className='links-container' ref={linkContainerRef}>
+                    <ul className='links' ref={linksRef}>
                         {links.map(link => {
                             const { id, url, text } = link;
                             return (
@@ -25,6 +41,7 @@ const Navbar = () => {
                         })}
                     </ul>
                 </div>
+                )
                 <ul className='social-icons'>
                     {social.map(socials => {
                         const { id, url, icon } = socials;
