654ea6d51a0e9ef818db4da0e658cef48d2b7a3d
Victor Irekponor <marquisvictor93@gmail.com>
2021-08-01 23:59:30 +0100

    commit before 12

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,57 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+    const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-export default Navbar
+    const displaySubmenu = () => {
+        openSubmenu();
+    };
+    return (
+        <nav className='nav'>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                    <img
+                        src={logo}
+                        alt='stripe you b*tches'
+                        className='nav-logo'
+                    />
+                    <button onClick={openSidebar} className='btn toggle-btn'>
+                        <FaBars />
+                    </button>
+                </div>
+                <ul className='nav-links'>
+                    <li>
+                        <button
+                            onMouseOver={displaySubmenu}
+                            className='link-btn'
+                        >
+                            Products
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onMouseOver={displaySubmenu}
+                            className='link-btn'
+                        >
+                            Developers
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onMouseOver={displaySubmenu}
+                            className='link-btn'
+                        >
+                            Company
+                        </button>
+                    </li>
+                </ul>
+                <button className='btn signin-btn'>Sign in </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
